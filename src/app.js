18e9f67678754ffad4f03c61b3911485288d1162
chinghok <cheachinghok@gmail.com>
2026-04-20 10:47:49 +0700

    Refactor CORS configuration to use an array for allowed origins

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ app.use(compression());
 
 // CORS
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000'|| 'https://1092-store-admin.vercel.app',
+  origin: ['http://localhost:3000', 'https://1092-store-admin.vercel.app'],
   credentials: true,
 }));
 
